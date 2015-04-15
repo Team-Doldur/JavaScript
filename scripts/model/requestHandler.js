aaf7c2000b02d0ddbcb05cd03f54a6f8e0b31011
Emil-Petrov <Emil.petrov95@mail.bg>
2015-04-15 19:29:44 +0300

    Added user login and registration functionality

--- scripts/model/requestHandler.js
+++ scripts/model/requestHandler.js
@@ -1,1 +1,19 @@
 var app = app || {};
+var appId, RESTApiKey;
+appId = 'HvWzY9d2JMZkKx37DHCDXmrTWFMVtqHbFWdPaIsn';
+RESTApiKey = 'ZoKpEeNBk0reOJJTkT4QxCwYzvMkqokNasyoDLhH';
+function sendRequest(method, url, data, token, success, error) {
+	$.ajax({
+		async: false,
+		type: method,
+		url: url,
+		headers: {
+			'X-Parse-Application-Id': appId,
+			'X-Parse-REST-API-Key': RESTApiKey,
+			'X-Parse-Session-Token': token
+		},
+		data: JSON.stringify(data),
+		success: success,
+		error: error
+	});
+}
