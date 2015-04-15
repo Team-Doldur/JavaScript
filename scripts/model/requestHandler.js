c0caa94139c35da50eeb6e5b57a4482e39764dce
GGeorgieva <georgieva.g.gt@gmail.com>
2015-04-15 16:27:54 +0300

    Add request handler for communication with Parse.com.

--- scripts/model/requestHandler.js
+++ scripts/model/requestHandler.js
@@ -1,1 +1,41 @@
 var app = app || {};
+
+app.requestHandler = (function () {
+    function makeRequest(method, url, data, success, error) {
+        $.ajax({
+            method: method,               
+            headers: {
+                'X-Parse-Application-Id': 'HvWzY9d2JMZkKx37DHCDXmrTWFMVtqHbFWdPaIsn',
+                'X-Parse-REST-API-Key': 'ZoKpEeNBk0reOJJTkT4QxCwYzvMkqokNasyoDLhH'
+            },
+            url: url,
+            data: JSON.stringify(data),
+            contentType: 'application/json',
+            success: success,
+            error: error
+        })
+    }
+        
+    function getRequest(url, success, error) {
+        makeRequest('GET', url, null, success, error);
+    }
+        
+    function postRequest(url, data, success, error) {
+        makeRequest('POST', url, data, success, error);
+    }
+        
+    function deleteRequest(url, success, error) {
+        makeRequest('DELETE', url, null, success, error);
+    }
+
+    function editRequest(url, success, error) {
+        makeRequest('PUT', url, data, success, error);
+    }    
+
+    return {
+            getRequest : getRequest,
+            postRequest : postRequest,
+            deleteRequest : deleteRequest,
+            editRequest : editRequest
+        }
+})();
