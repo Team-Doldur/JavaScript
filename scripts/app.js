1f8a972b4b408891fe19cb7fd85debd8d0e25e43
Ilian Kostov <ilian.kostov@gmail.com>
2015-04-16 23:09:17 +0300

    Refactor skeleton

--- scripts/app.js
+++ scripts/app.js
@@ -4,30 +4,35 @@ requirejs.config({
     paths: {
         // libs
         jquery: 'lib/jquery-2.1.3.min',
+        lodash: 'lib/lodash.min',
+        mustache: 'lib/mustache.min',
+        q: 'lib/q',
+        sammy: 'lib/sammy-latest.min',
 
-        // controllers
-        albumController: 'controller/album',
-        categoryController: 'controller/category',
-        photoController: 'controller/photo',
+        // controller
+        controller: 'controller',
 
         // models
         albumModel: 'model/album',
         categoryModel: 'model/category',
+        commentModel: 'model/comment',
         photoModel: 'model/photo',
+        rateModel: 'model/rate',
         requestHandler: 'model/requestHandler',
         userModel: 'model/user',
 
         // views
         albumView: 'view/album',
         categoryView: 'view/category',
-        photoView: 'view/photo'
+        commentView: 'view/comment',
+        headerView: 'view/header',
+        photoView: 'view/photo',
+        rateView: 'view/rate'
     }
 });
 
 var app = app || {};
 
-define(['jquery', 'userModel'], function () {
-    return (function() {
-
-    })();
+define(['jquery', 'sammy', 'q', 'lodash', 'mustache'], function () {
+    //TODO: implement sammy router
 });
