4446e8d3a375984483162e74f8a60b010cd7bdc3
GGeorgieva <georgieva.g.gt@gmail.com>
2015-04-18 12:53:48 +0300

    Refactored requestHandler.js

--- scripts/model/requestHandler.js
+++ scripts/model/requestHandler.js
@@ -5,22 +5,44 @@ define(['q'], function (Q) {
         function RequestHandler(baseUrl) {
             this._baseUrl = baseUrl;
         }
-
+        
         RequestHandler.prototype.getRequest = function (serviceUrl) {
             var headers = getHeaders();
             var url = this._baseUrl + serviceUrl;
-
+            
             return makeRequest('GET', headers, url);
         };
-
-        function makeRequest(method, headers, url, data) {
+        
+        RequestHandler.prototype.postRequest = function (serviceUrl, data, contentType) {
+            var headers = getHeaders();
+            var url = this._baseUrl + serviceUrl;
+            
+            return makeRequest('POST', headers, url, data, contentType);
+        }
+        
+        RequestHandler.prototype.deleteRequest = function (serviceUrl) {
+            var headers = getHeaders();
+            var url = this._baseUrl + serviceUrl;
+            
+            return makeRequest('DELETE', headers, url);
+        }
+        
+        RequestHandler.prototype.editRequest = function (serviceUrl, data, contentType) {
+            var headers = getHeaders();
+            var url = this._baseUrl + serviceUrl;
+            
+            return makeRequest('PUT', headers, url, data, contentType);
+        }
+        
+        function makeRequest(method, headers, url, data, contentType) {
             var deffer = Q.defer();
-
+            
             $.ajax({
                 method: method,
                 headers: headers,
                 url: url,
                 data: JSON.stringify(data),
+                contentType: contentType,
                 success: function (data) {
                     deffer.resolve(data);
                 },
@@ -28,24 +50,23 @@ define(['q'], function (Q) {
                     deffer.reject(error);
                 }
             });
-
+            
             return deffer.promise;
         }
-
+        
         function getHeaders() {
             var headers = {
                 'X-Parse-Application-Id' : 'HvWzY9d2JMZkKx37DHCDXmrTWFMVtqHbFWdPaIsn',
-                'X-Parse-REST-API-Key' : 'ZoKpEeNBk0reOJJTkT4QxCwYzvMkqokNasyoDLhH',
-                'Content-Type' : 'application/json'
+                'X-Parse-REST-API-Key' : 'ZoKpEeNBk0reOJJTkT4QxCwYzvMkqokNasyoDLhH',                
             };
-
-            if(sessionStorage['logged-in']) {
+            
+            if (sessionStorage['logged-in']) {
                 headers['X-Parse-Session-Token'] = sessionStorage['logged-in'];
             }
-
+            
             return headers;
         }
-
+        
         return {
             load: function (baseUrl) {
                 return new RequestHandler(baseUrl);
