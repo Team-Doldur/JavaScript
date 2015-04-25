46c821e59e2e67e826de48a87e2dc7d8e896371d
Petar <mbg012@gmail.com>
2015-04-25 20:22:23 +0300

    Comment model
    getAlbumComments and getPictureComments methods added

--- scripts/model/comment.js
+++ scripts/model/comment.js
@@ -1,29 +1,32 @@
 define(['q', 'requestHandler'], function (Q, requestHandler) {
-    var serviceUrl = 'classes/Comment';
+    var serviceUrl = '/classes/Comment';
 
     function Comment(baseUrl) {
-
+        this._requestHandler = requestHandler.load(baseUrl);
     }
 
-    Comment.prototype._getComments = function (resourceType, resourceId) {
-
+    Comment.prototype._getComments = function(resourceType, resourceId) {
+        return this._requestHandler.getRequest(serviceUrl + '?where=' + JSON.stringify({
+                resourceType: resourceType,
+                resourceId : resourceId
+            }));
     };
 
-    Comment.prototype.getAlbumComments = function () {
-
+    Comment.prototype.getAlbumComments = function (id) {
+        return this._getComments('Album', id);
     };
 
-    Comment.prototype.getPhotoComments = function () {
-
+    Comment.prototype.getPictureComments = function (id) {
+        return this._getComments('Picture', id);
     };
 
     Comment.prototype.postComment = function () {
-        
+
     };
 
     return {
-        load: function () {
-            return new Comment();
+        load: function (baseUrl) {
+            return new Comment(baseUrl);
         }
     }
 });
--- scripts/model/modelsLoader.js
+++ scripts/model/modelsLoader.js
@@ -1,9 +1,10 @@
-define(['categoryModel', 'albumModel', 'photoModel'], function (categoryModel, albumModel, photoModel){
+define(['categoryModel', 'albumModel', 'photoModel', 'commentModel'], function (categoryModel, albumModel, photoModel, commentModel){
     return (function() {
         function Model(baseUrl) {
             this.categories = categoryModel.load(baseUrl);
             this.albums = albumModel.load(baseUrl);
             this.photos = photoModel.load(baseUrl);
+            this.comments = commentModel.load(baseUrl);
         }
 
         return {
