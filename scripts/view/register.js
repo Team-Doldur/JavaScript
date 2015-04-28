7f6cf5105a77400e2dfb6440844448e66cfdf824
Emil-Petrov <Emil.petrov95@mail.bg>
2015-04-29 01:41:06 +0300

    bugfix for page refreshing
    register form WIP

--- scripts/view/register.js
+++ scripts/view/register.js
@@ -1,14 +1,38 @@
 define(['mustache', 'userModel'], function (Mustache, userModel) {
-    return (function() {
+    return (function () {
         function RegisterView(selector) {
-            $.get('templates/register.tpl', function(template) {
+            $.get('templates/register.tpl', function (template) {
                 var output = Mustache.render(template);
                 $(selector).html(output);
-                $('.submit-btn').click(function(){
-                    var username = $('.username').val();
-                    var password = $('.password').val();
-                    var email = $('.email').val();
-                    userModel.register(username, password, email);
+                $('.submit-btn').click(function () {
+                    var notificationContainer, redirectTimeout, redirectMsg, username, password, email, interval;
+
+                    notificationContainer = $('.signup-form-wrap').children().first();
+                    username = $('.username').val();
+                    password = $('.password').val();
+                    email = $('.email').val();
+
+                    userModel.register(username, password, email)
+                        .then(function () {
+                            redirectTimeout = 3;
+                            redirectMsg = 'Redirecting to main page in ' + redirectTimeout;
+                            notificationContainer.html(redirectMsg);
+                            interval = setInterval(function () {
+                                redirectTimeout -= 1;
+                                redirectMsg = 'Redirecting to main page in ' + redirectTimeout;
+                                notificationContainer.html(redirectMsg);
+                                if (redirectTimeout <= 0) {
+                                    clearInterval(interval);
+                                    notificationContainer.html('Redirecting...');
+                                    setTimeout(function () {
+                                        window.location.replace('#/');
+                                    }, 1000);
+                                }
+                            }, 1000);
+                            notificationContainer.parent().noty({text: "Login success", type: 'success'})
+                        }, function () {
+                            notificationContainer.noty({text: "Invalid register parameters", type: 'error'})
+                        });
                 })
             })
         }
